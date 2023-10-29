bb09c21f19a36ab1c1f828d6274a0f4af20e6c37
Trioud <guillaume.crist@gmail.com>
2023-10-29 23:28:34 +0100

    feat: add page 10-29-2023

--- src/app/french/page.tsx
+++ src/app/french/page.tsx
@@ -1,4 +1,4 @@
-const FrenchChallengesLesson = () => {
+const FrenchTravelLesson = () => {
   const currentDate = new Date()
   const currentMonth = currentDate.toLocaleString('default', { month: 'long' })
   const currentDay = currentDate.getDate()
@@ -9,31 +9,28 @@ const FrenchChallengesLesson = () => {
       <div className="p-4 border rounded-md w-full bg-pink-300">
         <div className="text-center p-4 font-sans">
           <h1>{currentDay + ' ' + currentMonth + ' ' + currentYear}</h1>
-          <h2>{`Lesson on Challenges`}</h2>
-          <h2>{`Leçon sur les Défis`}</h2>
+          <h2>{`Lesson on Traveling`}</h2>
+          <h2>{`Leçon sur les Voyages`}</h2>
         </div>
         <div className="font-mono">
-          <div className="text-white-300">{`Challenges are obstacles or difficulties that people face`}</div>
-          <div className="text-red-600">{`Les défis sont des obstacles ou des difficultés auxquels les gens sont confrontés`}</div>
+          <div className="text-white-300">{`Traveling is the act of going from one place to another, typically over a considerable distance`}</div>
+          <div className="text-red-600">{`Voyager, c'est l'acte de se déplacer d'un endroit à un autre, généralement sur une longue distance`}</div>
 
-          <div className="text-white-300">{`Challenges can come in various forms, such as personal, professional, or academic challenges`}</div>
-          <div className="text-red-600">{`Les défis peuvent prendre diverses formes, comme des défis personnels, professionnels ou académiques`}</div>
+          <div className="text-white-300">{`Traveling allows people to explore new destinations, experience different cultures, and create lasting memories`}</div>
+          <div className="text-red-600">{`Les voyages permettent aux personnes de découvrir de nouvelles destinations, de vivre différentes cultures et de créer des souvenirs durables`}</div>
 
-          <div className="text-white-300">{`Overcoming challenges often requires determination, problem-solving, and resilience`}</div>
-          <div className="text-red-600">{`Surmonter les défis nécessite souvent de la détermination, de la résolution de problèmes et de la résilience`}</div>
+          <div className="text-white-300">{`When traveling, it's essential to plan and prepare, including booking accommodation, transportation, and researching the destination`}</div>
+          <div className="text-red-600">{`Lorsque l'on voyage, il est essentiel de planifier et de se préparer, y compris la réservation de l'hébergement, des transports, et la recherche sur la destination`}</div>
 
-          <div className="text-white-300">{`Facing challenges can lead to personal growth and development`}</div>
-          <div className="text-red-600">{`Affronter des défis peut conduire à la croissance personnelle et au développement`}</div>
+          <div className="text-white-300">{`Traveling can be a source of personal growth, broadening one's horizons and gaining new perspectives`}</div>
+          <div className="text-red-600">{`Voyager peut être une source de croissance personnelle, élargissant ses horizons et acquérant de nouvelles perspectives`}</div>
 
-          <div className="text-white-300">{`It's important to have a positive attitude when dealing with challenges`}</div>
-          <div className="text-red-600">{`Il est important d'avoir une attitude positive lorsque l'on fait face à des défis`}</div>
-
-          <div className="text-white-300">{`Share your most significant challenge and how you overcame it on WeChat, in French`}</div>
-          <div className="text-red-600">{`Partagez votre défi le plus significatif et comment vous l'avez surmonté sur WeChat, en français`}</div>
+          <div className="text-white-300">{`Share your most memorable travel experience and what you learned from it on WeChat, in French`}</div>
+          <div className="text-red-600">{`Partagez votre expérience de voyage la plus mémorable et ce que vous avez appris grâce à elle sur WeChat en français`}</div>
         </div>
       </div>
     </div>
   )
 }
 
-export default FrenchChallengesLesson
+export default FrenchTravelLesson
